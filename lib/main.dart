421a51618407efe6a33a4c771e67f21e9a8b125c
vicyoslife <felipendc10phoenix@gmail.com>
2024-06-07 05:19:11 -0300

    // Set the status bar color to match the app theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: avoid_print
 
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 import 'package:vicyos_music_player/app/common/color_extension.dart';
@@ -8,8 +9,16 @@ import 'package:vicyos_music_player/app/controller/home.controller.dart';
 import 'package:vicyos_music_player/app/view/main_player_view.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   Get.put(HomeController());
   runApp(const MyApp());
+
+  // Set the status bar color to match the app theme
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors
+        .transparent, // Set to transparent if you want the status bar to blend with the app
+  ));
 }
 
 class MyApp extends StatelessWidget {
