2e4a3d6d7c86cc1543ee960401f559ad3f79088b
vicyoslife <felipendc10phoenix@gmail.com>
2024-06-09 17:06:25 -0300

    Change the app name on the recents screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Music Player',
+      title: 'Vicyos Music Player',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: "Circular Std",
