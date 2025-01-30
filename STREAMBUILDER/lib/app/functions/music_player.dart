44a1511a599fa05b1c6d06fe0f0dfdaddac3b022
vicyoslife <felipendc10phoenix@gmail.com>
2025-01-30 14:32:56 -0300

    Fix Mini Player won't update after disposing the Song Preview Screen

--- STREAMBUILDER/lib/app/functions/music_player.dart
+++ STREAMBUILDER/lib/app/functions/music_player.dart
@@ -64,6 +64,9 @@ double sleekCircularSliderPositionPreview = 0.0;
 double sleekCircularSliderDurationPreview = 100.0;
 //
 
+StreamController<void> miniPlayerStreamController =
+    StreamController<void>.broadcast();
+
 StreamController<String> getCurrentSongFolderStreamController =
     StreamController<String>.broadcast();
 
@@ -93,6 +96,10 @@ StreamController<double> systemVolumeStreamController =
 
 StreamController<bool> albumArtStreamController =
     StreamController<bool>.broadcast();
+
+void miniPlayerStreamControllerListener() {
+  miniPlayerStreamController.sink;
+}
 
 void listPlaylistFolderStreamListener() {
   listPlaylistFolderStreamController.sink
