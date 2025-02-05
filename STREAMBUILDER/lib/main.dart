456544d48362ff5bcd4add293f6e6e79b67a4918
vicyoslife <felipendc10phoenix@gmail.com>
2025-02-05 08:37:30 -0300

    Keep cleaning the Cache and Image cashe...

--- STREAMBUILDER/lib/main.dart
+++ STREAMBUILDER/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 import 'package:just_audio_background/just_audio_background.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:vicyos_music/app/common/color_extension.dart';
 import 'package:vicyos_music/app/view/home.screen.dart';
 
@@ -27,8 +29,46 @@ void main() async {
   ));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    clearCache(); // Clears the cache when the app starts
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.paused) {
+      clearCache(); // Clears the cache when the app goes to the background
+    } else if (state == AppLifecycleState.resumed) {
+      clearCache(); // Clears the cache when the app comes back to the foreground
+    } else if (state == AppLifecycleState.detached) {
+      clearCache(); // Clears the cache when the app is closed (detached)
+    }
+  }
+
+  Future<void> clearCache() async {
+    await DefaultCacheManager().emptyCache();
+    final tempDir = await getTemporaryDirectory();
+    if (tempDir.existsSync()) {
+      tempDir.deleteSync(recursive: true);
+      print("Cache cleared!");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
