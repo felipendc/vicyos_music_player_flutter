3d6322bf3a0401e6afc15322a003fbd8ad504c68
vicyoslife <felipendc10phoenix@gmail.com>
2025-09-18 23:59:31 -0300

    Add support to XIAOMI PAD 6 Tablet

---
+++ VICYOS_APP_FOR_TABLET_XIAOMI_PAD_6/lib/app/view/bottomsheet/bottom.sheet.import.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:vicyos_music/app/common/color_extension.dart';
+import 'package:vicyos_music/app/functions/music_player.dart';
+
+class ImportFilesBottomSheet extends StatelessWidget {
+  const ImportFilesBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: const BorderRadius.vertical(
+        top: Radius.circular(25),
+        bottom: Radius.circular(0),
+      ),
+      child: Container(
+        color: TColor.bg,
+        height: 300, // Adjust the height as needed
+        padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // Top button indicator
+            Container(
+              width: 100,
+              margin: const EdgeInsets.only(top: 10, bottom: 10),
+              height: 5,
+              decoration: BoxDecoration(
+                color: TColor.secondaryText,
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "ADD TO PLAYLIST",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                color: TColor.org,
+                fontSize: 19,
+              ),
+            ),
+            const SizedBox(height: 20),
+
+            Expanded(
+              child: Container(
+                color: TColor.bg,
+                child: ListView(
+                  children: [
+                    Material(
+                      color: Colors.transparent,
+                      child: ListTile(
+                        leading: Padding(
+                          padding: const EdgeInsets.only(left: 17),
+                          child: Icon(
+                            Icons.create_new_folder_outlined,
+                            color: TColor.focus,
+                            size: 38,
+                          ),
+                        ),
+                        title: Text(
+                          "Import folder",
+                          style: TextStyle(
+                            color: TColor.primaryText80,
+                            fontSize: 19,
+                          ),
+                        ),
+                        contentPadding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+                        onTap: () async {
+                          await pickFolder();
+                        },
+                      ),
+                    ),
+                    const Divider(
+                      color: Colors.white12,
+                      indent: 70,
+                      endIndent: 25,
+                      height: 1,
+                    ),
+                    Material(
+                      color: Colors.transparent,
+                      child: ListTile(
+                        leading: Padding(
+                          padding: const EdgeInsets.only(left: 17),
+                          child: Icon(
+                            Icons.queue_music_rounded,
+                            color: TColor.focus,
+                            size: 40,
+                          ),
+                        ),
+                        title: Text(
+                          "Add songs",
+                          style: TextStyle(
+                            color: TColor.primaryText80,
+                            fontSize: 19,
+                          ),
+                        ),
+                        contentPadding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+                        onTap: () async {
+                          await pickAndPlayAudio();
+                        },
+                      ),
+                    ),
+                    const Divider(
+                      color: Colors.white12,
+                      indent: 70,
+                      endIndent: 25,
+                      height: 1,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
