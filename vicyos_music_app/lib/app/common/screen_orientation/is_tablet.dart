d48d4bce5e16f01220771b0803434e2355656d48
vicyoslife <felipendc10phoenix@gmail.com>
2025-10-14 20:51:39 -0300

    update typo

--- vicyos_music_app/lib/app/common/screen_orientation/is_tablet.dart
+++ vicyos_music_app/lib/app/common/screen_orientation/is_tablet.dart
@@ -1,8 +1,9 @@
 import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 bool isTablet(BuildContext context) {
   final size = MediaQuery.of(context).size;
   final diagonal = sqrt(size.width * size.width + size.height * size.height);
-  return diagonal > 1100; // limit in pixels, ajustable
+  return diagonal > 1100; // limit in pixels, adjustable
 }
