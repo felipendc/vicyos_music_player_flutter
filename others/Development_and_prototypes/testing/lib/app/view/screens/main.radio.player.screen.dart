a8a665eb86cce34db632c43c887dbac78ce25b4e
felipendc <felipendc10phoenix@gmail.com>
2025-12-31 03:04:16 -0300

    fix radio  reloading going back one index

--- others/Development_and_prototypes/testing/lib/app/view/screens/main.radio.player.screen.dart
+++ others/Development_and_prototypes/testing/lib/app/view/screens/main.radio.player.screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
 import 'package:vicyos_music/app/color_palette/color_extension.dart';
+import 'package:vicyos_music/app/components/marquee.text.dart';
 import 'package:vicyos_music/app/music_player/music.player.functions.and.more.dart';
 import 'package:vicyos_music/app/music_player/music.player.stream.controllers.dart';
 import 'package:vicyos_music/app/radio_player/bottomsheet/radio.bottom.sheet.speed.rate.dart';
@@ -11,7 +12,6 @@ import 'package:vicyos_music/app/radio_player/widgets/radio.appbar.dart';
 import 'package:vicyos_music/app/radio_player/widgets/radio.music.visualizer.dart';
 import 'package:vicyos_music/app/radio_player/widgets/show.radio.top.message.dart';
 import 'package:vicyos_music/app/screen_orientation/screen.orientation.dart';
-import 'package:vicyos_music/app/components/marquee.text.dart';
 import 'package:vicyos_music/l10n/app_localizations.dart';
 import 'package:wave_progress_widget/wave_progress.dart';
 
@@ -269,7 +269,7 @@ class MainRadioPlayerView extends StatelessWidget {
                                       reLoadRatioStationCurrentIndex(context);
                                     } else {
                                       playRadioStation(
-                                          context, (currentRadioIndex - 1));
+                                          context, (currentRadioIndex));
                                     }
                                   }
                                 },
